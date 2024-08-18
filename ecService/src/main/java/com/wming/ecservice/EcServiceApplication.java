631fe568899617c67940f11e18adffa534131067
Minje Kim <149574203+mingMy-00@users.noreply.github.com>
2024-08-18 14:15:57 +0900

    환경설정

---
+++ ecService/src/main/java/com/wming/ecservice/EcServiceApplication.java
@@ -0,0 +1,14 @@
+package com.wming.ecservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.plaf.basic.BasicDesktopIconUI;
+
+@SpringBootApplication
+public class EcServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcServiceApplication.class, args);
+    }
+
+}
