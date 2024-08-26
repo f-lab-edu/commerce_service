300bbd2c9b24a50cfa3ccdc17f2c5005e90bc3cf
Minje Kim <149574203+mingMy-00@users.noreply.github.com>
2024-08-27 07:28:05 +0900

    [feat]-김민제-[커스텀 에러 + 글로벌 처리]

--- ecService/src/main/java/com/wming/ecservice/order/controller/OrderController.java
+++ ecService/src/main/java/com/wming/ecservice/order/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.wming.ecservice.order.controller;
 
+import com.wming.ecservice.common.response.ApiResponse;
 import com.wming.ecservice.order.dto.OrderRequest;
 import com.wming.ecservice.order.service.OrderSerivce;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +21,11 @@ public class OrderController {
     this.orderSerivce = orderSerivce;
   }
 
-  @PostMapping("/createOrder")
-  public String createOrder(@RequestBody OrderRequest orderRequest) {
+  @PostMapping(value = "/createOrder", produces = "application/json")
+  public ResponseEntity<ApiResponse<String>> createOrder(@RequestBody OrderRequest orderRequest) {
     // 주문 정보 받는 거는  (상품 정보 product ,
     orderSerivce.createOrder(orderRequest);
-    return "주문이 완료되었습니다.";
+    return ResponseEntity.ok(ApiResponse.successString("주문이 완료 되었습니다."));
   }
 }
 
